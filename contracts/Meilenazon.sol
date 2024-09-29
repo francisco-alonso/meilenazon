c1288b78426f5729b78d4996a624282bf7bd691a
Francisco <fraloal97@gmail.com>
2024-09-29 17:06:12 +0200

    list item

--- contracts/Meilenazon.sol
+++ contracts/Meilenazon.sol
@@ -3,8 +3,44 @@ pragma solidity ^0.8.9;
 
 contract Meilenazon {
     string public name;
-    
+    address public owner;
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
     constructor() {
-        name = "MeilenAzon";
+        owner = msg.sender; // state variable to get th person who is deploying the smart contract qnd signing the transaction
+    }
+
+    // List products
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        items[_id] = item;
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 function App() {
-
   return (
     <div>
-
       <h2>Welcome to Meilenazon</h2>
-
     </div>
   );
 }
